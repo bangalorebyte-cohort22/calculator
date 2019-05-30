7f895a2cd19be6d306a6cf921c0f02e395381c27
RuchirChawdhry <ruchirkc@gmail.com>
2019-05-30 13:41:36 +0530

    blabla

--- calculator.py
+++ calculator.py
@@ -7,7 +7,15 @@ def user_input():
     return user_input.split(" ")
 
 def input_check(user_input):
-    if user_input[0] in ['div', 'mul', 'add', 'sub']
+    if len(user_input)!=3:
+        print("Please enter either: div, mul, add, sub and 2 numbers separated with spaces")
+        raise "Error"
+    # if user_input[1] == str.__contai
+    if "." in user_input[1] and user_input[2]:
+        print ("Not a whole number. Please enter a whole number.")
+    # if user_input
+    # user_input[0] in ['div', 'mul', 'add', 'sub']
+
 
 def add():
     pass
